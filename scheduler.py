58bf724394a0bbb200a8f67144531688e01caa54
Jordan Clemens <jorcleme@cisco.com>
2024-06-07 16:07:42 -0400

    scheduler missing start() and reactor event never started

--- scheduler.py
+++ scheduler.py
@@ -13,6 +13,8 @@ def send_request():
 
 
 if __name__ == "__main__":
-    subprocess.run("scrapyd-deploy heroku", shell=True, universal_newlines=True)
+    subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     scheduler = TwistedScheduler(timezone=pytz.timezone("US/Eastern"))
     scheduler.add_job(send_request, "cron", day_of_week="mon", hour=3, minute=0)
+    scheduler.start()
+    reactor.run()
